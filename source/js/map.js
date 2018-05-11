c7274f0a290bca06c5412c9c2db2017d7ef10325
maximkatut <maximkatut@gmail.com>
2018-05-11 17:55:42 -0400

    фиксики пришли

--- source/js/map.js
+++ source/js/map.js
@@ -3,17 +3,24 @@
 var ZOOM = 7;
 var isIE11 = !!(navigator.userAgent.match(/Trident/) && navigator.userAgent.match(/rv[ :]11/));
 var image;
+var mapBlock = document.querySelector('.map');
 
 if (isIE11) {
-  image = 'img/map-marker.png';
+  image = "img/map-marker.png";
 } else {
-  image = 'img/icon-map-marker.svg';
+  image = "img/icon-map-marker.svg";
 }
+
+function setStyleMap() {
+  mapBlock.style.width = "99.225vw";
+  mapBlock.style.marginLeft = "-49.59vw";
+  mapBlock.style.left = "50%";
+};
 
 function initMap() {
   var uluru = {lat: 34.8713762, lng: -111.7648569};
   var gllacy = {lat: 34.8713762, lng: -111.7648569};
-  var map = new google.maps.Map(document.querySelector('.map'), {
+  var map = new google.maps.Map(mapBlock, {
     zoom: ZOOM,
     center: uluru
   });
@@ -23,3 +30,5 @@ function initMap() {
     icon: image
   });
 };
+
+setStyleMap();
