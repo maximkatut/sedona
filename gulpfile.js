72af5e2c237b025f21527c5bbc53ea7a23684760
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-08-30 20:51:02 -0400

    update package-lock for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var del = require("del");
 var run = require("run-sequence");
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
@@ -15,16 +14,15 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
-var minifyjs = require('gulp-js-minify');
-var concat = require('gulp-concat');
+var minifyjs = require("gulp-js-minify");
+var concat = require("gulp-concat");
 
-gulp.task("style", function() {
-  gulp.src("source/sass/style.scss")
+gulp.task("style", function () {
+  gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
@@ -33,56 +31,64 @@ gulp.task("style", function() {
 });
 
 gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
+  return gulp
+    .src("source/img/**/*.{png,jpg,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.svgo(),
+      ])
+    )
     .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 80}))
+  return gulp
+    .src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 80 }))
     .pipe(gulp.dest("source/img/webp"));
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
+  return gulp
+    .src("source/img/icon-*.svg")
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task('minify-js', function(){
-  gulp.src('build/js/map.js')
+gulp.task("minify-js", function () {
+  gulp
+    .src("build/js/map.js")
     .pipe(minifyjs())
-    .pipe(rename('map.min.js'))
-    .pipe(gulp.dest('build/js'));
-  gulp.src('build/js/menu.js')
+    .pipe(rename("map.min.js"))
+    .pipe(gulp.dest("build/js"));
+  gulp
+    .src("build/js/menu.js")
     .pipe(minifyjs())
-    .pipe(rename('menu.min.js'))
-    .pipe(gulp.dest('build/js'));
+    .pipe(rename("menu.min.js"))
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
+  return gulp
+    .src("source/*.html")
+    .pipe(posthtml([include()]))
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("serve", function() {
+gulp.task("serve", function () {
   server.init({
     server: "build/",
     notify: false,
     open: true,
     cors: true,
-    ui: false
+    ui: false,
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
@@ -90,23 +96,15 @@ gulp.task("serve", function() {
 });
 
 gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
+  return gulp
+    .src(["source/fonts/**/*.{woff,woff2}", "source/img/**", "source/js/**"], {
+      base: "source",
+    })
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("build", function (done) {
   run(
-    "clean",
     "copy",
     "style",
     // "sprite",
